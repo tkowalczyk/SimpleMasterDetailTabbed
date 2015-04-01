0b6f68b1d830f7417334aa122dd4a731d7fbc90a
Tomasz Kowalczyk <tomek@kownet.info>
2015-04-01 19:10:38 +0200

    image added

--- SimpleMasterDetailTabbed/SimpleMasterDetailTabbed/Views/FeedbackView.cs
+++ SimpleMasterDetailTabbed/SimpleMasterDetailTabbed/Views/FeedbackView.cs
@@ -7,11 +7,42 @@ namespace SimpleMasterDetailTabbed
 	{
 		public FeedbackView ()
 		{
-			this.Content = new Label {
-				Text = "Feedback View!",
-				VerticalOptions = LayoutOptions.CenterAndExpand,
-				HorizontalOptions = LayoutOptions.CenterAndExpand,
+			Grid mainGrid = new Grid()
+			{
+				VerticalOptions = LayoutOptions.Start,
+				HorizontalOptions = LayoutOptions.FillAndExpand,
+				RowSpacing = 0,
+				ColumnSpacing = 0,
+				Padding = 0,
+				RowDefinitions = 
+				{
+					new RowDefinition { Height = GridLength.Auto},
+					new RowDefinition { Height = GridLength.Auto}
+				},
+				ColumnDefinitions = 
+				{
+					new ColumnDefinition { Width = GridLength.Auto}
+				}
+				};
+
+			Image myImage = new Image()
+			{
+				Aspect = Aspect.AspectFill,
+				HeightRequest = 150,
+				MinimumHeightRequest = 150,
+				Source = ImageSource.FromFile(
+					"Image.png"
+				),
 			};
+
+			Label myLabel = new Label {
+				Text = "Feedback View!"
+			};
+
+			mainGrid.Children.Add(myImage, 0, 0);
+			mainGrid.Children.Add(myLabel, 0, 1);
+
+			this.Content = mainGrid;
 		}
 	}
 }
